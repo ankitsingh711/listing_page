b8d5d0769d1922e44b13a01e7de158b01561bbff
ankit711 <itsankitsingh711@edureka.co>
2022-03-05 08:28:41 +0530

    dark/light mode

--- home.js
+++ home.js
@@ -6,7 +6,7 @@ function closecoupon(){
     document.getElementById("coupon").style.visibility="hidden"
 }
 
-function changemode(){
-    var mybody = document.body
-    mybody.classList.toogle("mydarkmode")
+function changeMode(){
+    var mybody = document.body;
+    mybody.classList.toggle('darkmode')
 }
