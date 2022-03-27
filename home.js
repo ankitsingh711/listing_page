1108c78e94d6746b7131ebd5312ddd5b104cf1f9
ankit711 <itsankitsingh711@edureka.co>
2022-03-27 12:12:52 +0530

    json files

--- home.js
+++ home.js
@@ -11,3 +11,37 @@ function changeMode(){
     mybody.classList.toggle('darkmode')
 }
 
+const cityUrl = "https://zomatoajulypi.herokuapp.com/location";
+const restUrl = "https://zomatoajulypi.herokuapp.com/restaurant?stateId="
+
+const getCity = async() => {
+    let response = await fetch(cityUrl)
+    let data = await response.json()
+    data.map((item) => {
+        let element = document.createElement('option')
+        let text = document.createTextNode(item.state)
+        element.appendChild(text)
+        element.value = item.state_id;
+        document.getElementById('city').appendChild(element)
+    })
+}
+
+const getRest = () => {
+    const cityId = document.getElementById('city').value;
+    const rest =  document.getElementById('restaurant')
+
+    while(rest.length > 0){
+        rest.remove(0)
+    }
+
+    fetch(`${restUrl}${cityId}`)
+    .then((res) => res.json())
+    .then((data) => {
+        data.map((item) => {
+            let element = document.createElement('option') 
+            let text = document.createTextNode(`${item.restaurant_name}|${item.address}`) 
+            element.appendChild(text) 
+            rest.appendChild(element)
+        })
+    })
+}
